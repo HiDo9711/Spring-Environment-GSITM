f2ac57f19acd9f0a9a5632b6f4de0b636faa15d8
jklee12 <40033145+jklee12@users.noreply.github.com>
2024-06-21 18:04:05 +0900

    Setting Spring

---
+++ springProjectGSITM/src/main/java/com/mysite/gsitm/SpringProjectGsitmApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.gsitm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectGsitmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectGsitmApplication.class, args);
+	}
+
+}
---
+++ springProjectGSITM/src/test/java/com/mysite/gsitm/SpringProjectGsitmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.gsitm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectGsitmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
