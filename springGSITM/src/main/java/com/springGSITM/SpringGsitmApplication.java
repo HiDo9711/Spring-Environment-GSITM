b143e2ee3c291a09238fffa37a58d79c090cfa9a
jkl <jkl@DESKTOP-V5VR1HJ>
2024-06-23 14:48:44 +0900

    Spring Setting

---
+++ springGSITM/src/main/java/com/springGSITM/SpringGsitmApplication.java
@@ -0,0 +1,13 @@
+package com.springGSITM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGsitmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGsitmApplication.class, args);
+	}
+
+}
---
+++ springGSITM/src/test/java/com/springGSITM/SpringGsitmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springGSITM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGsitmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
