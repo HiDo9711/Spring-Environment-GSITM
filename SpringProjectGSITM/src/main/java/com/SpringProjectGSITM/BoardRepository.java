6064eca4c52101d3c94194368a326dc45cae1044
jklee12 <40033145+jklee12@users.noreply.github.com>
2024-06-25 11:56:39 +0900

    Delete SpringProjectGSITM directory

--- SpringProjectGSITM/src/main/java/com/SpringProjectGSITM/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.SpringProjectGSITM;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardRepository extends JpaRepository<Board, Integer> {
-}
