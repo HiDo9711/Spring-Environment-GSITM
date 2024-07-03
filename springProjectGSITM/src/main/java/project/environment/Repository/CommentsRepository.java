6bd44456e77db00679e7fe4b43aad4db6da69e80
Hi_Do <161135171+HiDo9711@users.noreply.github.com>
2024-07-03 16:59:48 +0900

    last commit

--- springProjectGSITM/src/main/java/project/environment/Repository/CommentsRepository.java
+++
@@ -1,9 +0,0 @@
-package project.environment.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import project.environment.entity.Comments;
-
-@Repository
-public interface CommentsRepository extends JpaRepository<Comments, Long> {
-}
